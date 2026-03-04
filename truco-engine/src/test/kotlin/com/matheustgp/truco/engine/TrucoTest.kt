5abd62ec44f026c3f50e8292ad14cf91b5cc594b
MatheusTGP ツ <matheustgpcn72@gmail.com>
2026-03-04 10:24:37 -0300

    feat(truco-engine): Add truco test package

---
+++ truco-engine/src/test/kotlin/com/matheustgp/truco/engine/TrucoTest.kt
@@ -0,0 +1,11 @@
+package com.matheustgp.truco.engine
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TrucoTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
